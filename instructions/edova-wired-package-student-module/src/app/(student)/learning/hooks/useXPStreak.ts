f57b9fff7d40ad2ef3e8ecf44ce46eddcdfbe4ea
PVS <tellsathish@gmail.com>
2026-07-29 06:57:59 +0530

    Delete instructions directory
    this folder must be gitignore

--- instructions/edova-wired-package-student-module/src/app/(student)/learning/hooks/useXPStreak.ts
+++
@@ -1,8 +0,0 @@
-"use client"
-import { useState } from "react"
-export function useXPStreak() {
-  const [xp, setXp] = useState(1240)
-  const [streak, setStreak] = useState(7)
-  const addXP = (v: number) => setXp(x=>x+v)
-  return { xp, streak, addXP }
-}
